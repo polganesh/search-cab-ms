a6f00bfb229ce887411bc0f1d6f018600fe41441
Ganesh Pol <polganesh@gmail.com>
2017-08-10 22:15:30 +0530

    add export related rules

--- src/main/java/com/gp/searchcab/repository/LocationCabTimeDetailRepository.java
+++ src/main/java/com/gp/searchcab/repository/LocationCabTimeDetailRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
@@ -36,136 +37,79 @@ public interface LocationCabTimeDetailRepository extends JpaRepository<LocationC
 			@Param("fromDate") @DateTimeFormat(iso = ISO.DATE) Date fromDate, @Param("locationId") Long locationId,
 			@Param("isdeleted") Byte isdeleted,@Param("hourforbooking")Integer hourforbooking);
 
-	// ----------------Blocked activities
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.repository.PagingAndSortingRepository#findAll(org.springframework.data.domain.Pageable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default Page<Locationcabcapacitytx> findAll(Pageable pageable) {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.repository.CrudRepository#delete(java.io.Serializable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void delete(Long id) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.repository.CrudRepository#delete(java.lang.Object)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void delete(Locationcabcapacitytx entity) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.repository.CrudRepository#delete(java.lang.Iterable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void delete(Iterable<? extends Locationcabcapacitytx> entities) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see org.springframework.data.repository.CrudRepository#deleteAll()
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void deleteAll() {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#deleteAllInBatch()
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void deleteAllInBatch() {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#deleteInBatch(java.lang.Iterable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default void deleteInBatch(Iterable<Locationcabcapacitytx> arg0) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see org.springframework.data.jpa.repository.JpaRepository#findAll()
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default List<Locationcabcapacitytx> findAll() {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#findAll(java.lang.Iterable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default List<Locationcabcapacitytx> findAll(Iterable<Long> arg0) {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#findAll(org.springframework.data.domain.Sort)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default List<Locationcabcapacitytx> findAll(Sort arg0) {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#save(java.lang.Iterable)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default <S extends Locationcabcapacitytx> List<S> save(Iterable<S> arg0)
-	// {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// /* (non-Javadoc)
-	// * @see
-	// org.springframework.data.jpa.repository.JpaRepository#saveAndFlush(java.lang.Object)
-	// */
-	// @Override
-	// @PreAuthorize("hasRole('ROLE_BLOCK_OPERATIONS')")
-	// default <S extends Locationcabcapacitytx> S saveAndFlush(S arg0) {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
+	
+		// ----------------Blocked activities
+	
+	@RestResource(exported=true)
+	@Override
+	default void delete(Iterable<? extends LocationCabTimeDetail> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@RestResource(exported=true)
+	@Override
+	default void delete(LocationCabTimeDetail arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@RestResource(exported=true)
+	@Override
+	default void delete(Long arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default boolean exists(Long arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default <S extends LocationCabTimeDetail> S save(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default void deleteInBatch(Iterable<LocationCabTimeDetail> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default <S extends LocationCabTimeDetail> List<S> save(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@RestResource(exported=true)
+	@Override
+	default <S extends LocationCabTimeDetail> S saveAndFlush(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
